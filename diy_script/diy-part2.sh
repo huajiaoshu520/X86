19946318e0d2c4141f31f9220118a7bc3accfc5e
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-03-25 06:36:37 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -66,3 +66,4 @@ sed -i 's/PKG_RELEASE:=8/PKG_RELEASE:=1/g' ./feeds/luci/applications/luci-app-lu
 sed -i 's/PKG_VERSION:=1.2.0/PKG_VERSION:=2.2.2/g' ./feeds/luci/applications/luci-app-lucky/Makefile
 sed -i 's/PKG_RELEASE:=1/PKG_RELEASE:=12/g' ./feeds/packages/net/lucky/Makefile
 sed -i 's/PKG_VERSION:=2.10.8/PKG_VERSION:=2.15.8/g' ./feeds/packages/net/lucky/Makefile
+sed -i 's/^LUCI_DEPENDS:=+lucky/LUCI_DEPENDS:=+lucky +luci-compat/' ./feeds/luci/applications/luci-app-lucky/Makefile
