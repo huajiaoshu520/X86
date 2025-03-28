7e8f6fbf2c03c5e14ec8abee29cbbdf5d720b2db
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-03-28 20:48:57 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -40,11 +40,18 @@ sed -i 's/echo $h/echo $g/g' ./package/lean/autocore/files/x86/autocore
 # dockerman
 #sed -i 's/+cgroupfs-mount //g' feeds/luci/applications/luci-app-dockerman/Makefile
 #sed -i '42i sed -i "/^# the system init finished. By default this file does nothing./a \/etc\/init.d\/cgroupfs-mount disable" \/etc\/rc.local' package/lean/default-settings/files/zzz-default-settings
-rm -rf ./feeds/luci/applications/luci-app-dockerman
-git clone https://github.com/Jason6111/luci-app-dockerman ./feeds/luci/applications/luci-app-dockerman
+#rm -rf ./feeds/luci/applications/luci-app-dockerman
+#git clone https://github.com/Jason6111/luci-app-dockerman ./feeds/luci/applications/luci-app-dockerman
 #rm -rf ./feeds/packages/utils/dockerd
 #git clone https://github.com/Jason6111/dockerd ./feeds/packages/utils/dockerd && chmod -R 777 ./feeds/packages/utils/dockerd
-
+#dockerd
+sed -i -e 's/27.0.2/28.0.4/g' \
+       -e 's/2705abdd7a8a47ec88c0d2f0f31eede01ba4aa5ae8b5ab0e2c4ee25e7c314521/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/g' \
+       -e 's/e953d76/c710b88/g' ./feeds/packages/utils/dockerd/Makefile
+#docker
+sed -i -e 's/27.0.2/28.0.4/g' \
+       -e 's/a2b31d4c8143c8b126e98a359639f51727fc83fc1e61670efbdeaa7dea92fd5a/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/g' \
+       -e 's/912c1dd/b8034c0/g' ./feeds/packages/utils/docker/Makefile
 # 临时
 sed -i 's/6.6/6.12/g'  ./target/linux/x86/Makefile
 
