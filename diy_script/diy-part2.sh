96750c31e4ff059148828314833d04a2e2667a73
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-03-29 08:03:33 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -47,7 +47,7 @@ sed -i 's/echo $h/echo $g/g' ./package/lean/autocore/files/x86/autocore
 #dockerd
 sed -i -e 's/27.0.2/28.0.4/g' \
        -e 's/2705abdd7a8a47ec88c0d2f0f31eede01ba4aa5ae8b5ab0e2c4ee25e7c314521/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/g' \
-       -e 's/e953d76/42c8b31/g' ./feeds/packages/utils/dockerd/Makefile
+       -e 's/e953d76/6430e49/g' ./feeds/packages/utils/dockerd/Makefile
 #docker
 sed -i -e 's/27.0.2/28.0.4/g' \
        -e 's/a2b31d4c8143c8b126e98a359639f51727fc83fc1e61670efbdeaa7dea92fd5a/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/g' \
