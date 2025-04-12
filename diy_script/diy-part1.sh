616aff2fa91f25786148fe0e6169f1b507d22ba3
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-04-12 09:53:33 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -21,3 +21,6 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+
+# Temp
+rm -rf ./target/linux/generic/hack-6.12/220-arm-gc_sections.patch
