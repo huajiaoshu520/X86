0699927b769acbbe9ef551f361b58c4a53e3f00e
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-06-25 15:34:27 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -67,3 +67,4 @@ sed -i '/^\[ -n "\$\${IPKG_INSTROOT}" \] \|\| echo "0 4 \* \* \* \/etc\/coremark
 #openlist
 sed -i 's/4.0.1/4.0.3/g'  ./feeds/packages/net/openlist/Makefile
 sed -i 's/a41798e4fcd56b6fa500e22558908de1cd1a4db6344170c933ecac803b81b4a5/f5bf709937c8f060ba1a47e937dbdb3f12e7690644a813cc7693ae028cbbc043/g'  ./feeds/packages/net/openlist/Makefile
+sed -i 's/0d52781014d01697e3f5af0131ae6f87124f06786188e543fe8b8a12bbeae7bd/26e30834fb36952b5383589a615bcdef6f2e34b29943fa7413351798b63a40e0/g'  ./feeds/packages/net/openlist/Makefile
