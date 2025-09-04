454ce9814454ce09af437cf994cc304c7f2b602a
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-09-04 16:35:49 +0800

    Update diy-docker.sh

--- diy_script/diy-docker.sh
+++ diy_script/diy-docker.sh
@@ -21,22 +21,22 @@
 # dockerd
 # wget https://codeload.github.com/moby/moby/tar.gz/v28.3.0
 # sha256sum v28.1.1
-sed -i -e 's/28.0.4/28.3.3/g' \
-       -e 's/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/3727c8963ab4bcff12291e99e4d4a6b9a29ace5236fd245717bbff648f15f8cd/g' \
-       -e 's/6430e49/bea959c/g' ./feeds/packages/utils/dockerd/Makefile
+sed -i -e 's/28.0.4/28.4.0/g' \
+       -e 's/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/4aa0776ef3c040204cada69f3d4d7b89a3cef85e07cbe098c05cdcb000ea7e30/g' \
+       -e 's/6430e49/249d679/g' ./feeds/packages/utils/dockerd/Makefile
 
 # docker
 # wget https://codeload.github.com/docker/cli/tar.gz/v28.3.0
-sed -i -e 's/28.0.4/28.3.3/g' \
-       -e 's/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/172dca437abb36485275a8d43550db90b6878bcc676fc0b73a67a57a15026cff/g' \
-       -e 's/b8034c0/980b856/g' ./feeds/packages/utils/docker/Makefile
+sed -i -e 's/28.0.4/28.4.0/g' \
+       -e 's/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/420f9caf3ba09229cd98f7f8325fb9969d0746cccf4bdc3c56dfe2621bb07803/g' \
+       -e 's/b8034c0/d8eb465/g' ./feeds/packages/utils/docker/Makefile
 
 # containerd      
 # https://codeload.github.com/containerd/containerd
-sed -i -e 's/1.7.27/1.7.27/g' \
-       -e 's/374f1c906b409cfad142b20d208f99e9539e5eb47fbb47ea541b4dfc9867345f/374f1c906b409cfad142b20d208f99e9539e5eb47fbb47ea541b4dfc9867345f/g' ./feeds/packages/utils/containerd/Makefile
+sed -i -e 's/1.7.27/1.7.28/g' \
+       -e 's/374f1c906b409cfad142b20d208f99e9539e5eb47fbb47ea541b4dfc9867345f/546aa9fdb04a0cd40a5cbc5c931c269d42522d473abd7234b98d98e63316ed9b/g' ./feeds/packages/utils/containerd/Makefile
 
 # runc
 # https://codeload.github.com/opencontainers/runc/tar.gz/v1.2.6
-sed -i -e 's/1.2.6/1.2.6/g' \
-       -e 's/19b280702341f33ff353fa254d1dbdb67f6aab2c74395f6d651a17524f68e752/19b280702341f33ff353fa254d1dbdb67f6aab2c74395f6d651a17524f68e752/g' ./feeds/packages/utils/runc/Makefile
+sed -i -e 's/1.2.6/1.3.0/g' \
+       -e 's/19b280702341f33ff353fa254d1dbdb67f6aab2c74395f6d651a17524f68e752/3262492ce42bea0919ee1a2d000b6f303fd14877295bc38d094876b55fdd448b/g' ./feeds/packages/utils/runc/Makefile
