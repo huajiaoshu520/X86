c564bfacc5e937abc8ed82faff790c38f2fc8ae3
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-11-24 20:08:56 +0800

    Update diy-docker.sh

--- diy_script/diy-docker.sh
+++ diy_script/diy-docker.sh
@@ -19,23 +19,23 @@
 #git clone https://github.com/Jason6111/dockerd ./feeds/packages/utils/dockerd && chmod -R 777 ./feeds/packages/utils/dockerd
 
 # dockerd
-# wget https://codeload.github.com/moby/moby/tar.gz/docker-v29.0.2
-# sha256sum v28.1.1
-sed -i -e 's/28.0.4/29.0.2/g' \
-       -e 's/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/7d7891d0c578d8118402d5f2fa353aab336d14dac96816a3dfb4f8f694b8d4d8/g' \
+# wget https://codeload.github.com/moby/moby/tar.gz/docker-v29.0.3
+# sha256sum docker-v29.0.3
+sed -i -e 's/28.0.4/29.0.3/g' \
+       -e 's/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/c12551cdc456969d96114971c606fe1090ec8197789f4ed8f5b1050773f7b377/g' \
        -e 's/PKG_GIT_REF:=v\$(PKG_VERSION)/PKG_GIT_REF:=docker-v$(PKG_VERSION)/' \
-       -e 's/6430e49/e9ff10b/g' ./feeds/packages/utils/dockerd/Makefile
+       -e 's/6430e49/1fa8a31/g' ./feeds/packages/utils/dockerd/Makefile
 
 # docker
-# wget https://codeload.github.com/docker/cli/tar.gz/v29.0.2
-sed -i -e 's/28.0.4/29.0.2/g' \
-       -e 's/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/95cf6a9dabd5eecdc77cab028d9b82ffc3ae54b36ffa72861599dae495e25c90/g' \
-       -e 's/b8034c0/8108357/g' ./feeds/packages/utils/docker/Makefile
+# wget https://codeload.github.com/docker/cli/tar.gz/v29.0.3
+sed -i -e 's/28.0.4/29.0.3/g' \
+       -e 's/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/def691b8d2a2aae4197d1f5f79529d36b14ae78e001ae41c0f32c50e606bb37a/g' \
+       -e 's/b8034c0/511dad6/g' ./feeds/packages/utils/docker/Makefile
 
 # containerd      
-# https://codeload.github.com/containerd/containerd/tar.gz/v2.2.0
-sed -i -e 's/1.7.27/2.1.5/g' \
-       -e 's/374f1c906b409cfad142b20d208f99e9539e5eb47fbb47ea541b4dfc9867345f/f8def69e02ecff84c07e24350be0b834a3a8e1f6accf042cae4d504e52eb03d1/g' ./feeds/packages/utils/containerd/Makefile
+# wget https://codeload.github.com/containerd/containerd/tar.gz/v2.2.0
+sed -i -e 's/1.7.27/2.2.0/g' \
+       -e 's/374f1c906b409cfad142b20d208f99e9539e5eb47fbb47ea541b4dfc9867345f/86e7a268fc73f5332522baef86082c1d6c17986e2957a9ad842ead35d1080fca/g' ./feeds/packages/utils/containerd/Makefile
 sed -i 's/containerd-shim,containerd-shim-runc-v1,//g' ./feeds/packages/utils/containerd/Makefile
 
 # runc
