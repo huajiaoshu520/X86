bb959db5bed0894e739bd7b83cf2f92f72c31f24
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-11-28 20:24:31 +0800

    Update diy-docker.sh

--- diy_script/diy-docker.sh
+++ diy_script/diy-docker.sh
@@ -34,7 +34,7 @@ sed -i -e 's/28.0.4/29.1.0/g' \
 
 # containerd      
 # wget https://codeload.github.com/containerd/containerd/tar.gz/v2.2.0
-sed -i -e 's/1.7.27/2.2.2/g' \
+sed -i -e 's/1.7.27/2.2.0/g' \
        -e 's/374f1c906b409cfad142b20d208f99e9539e5eb47fbb47ea541b4dfc9867345f/86e7a268fc73f5332522baef86082c1d6c17986e2957a9ad842ead35d1080fca/g' ./feeds/packages/utils/containerd/Makefile
 sed -i 's/containerd-shim,containerd-shim-runc-v1,//g' ./feeds/packages/utils/containerd/Makefile
 
