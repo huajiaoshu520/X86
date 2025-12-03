f9b4c61ca6a61be411000ede8ba8b601c08850a6
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2025-12-03 09:46:38 +0800

    Update diy-docker.sh

--- diy_script/diy-docker.sh
+++ diy_script/diy-docker.sh
@@ -19,18 +19,18 @@
 #git clone https://github.com/Jason6111/dockerd ./feeds/packages/utils/dockerd && chmod -R 777 ./feeds/packages/utils/dockerd
 
 # dockerd
-# wget https://codeload.github.com/moby/moby/tar.gz/docker-v29.1.1
-# sha256sum docker-v29.1.1
-sed -i -e 's/28.0.4/29.1.1/g' \
-       -e 's/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/65221f1c70feb1bd1562bb1017b586e4528be877656dc16f5be5659fc9b7e522/g' \
+# wget https://codeload.github.com/moby/moby/tar.gz/docker-v29.1.2
+# sha256sum docker-v29.1.2
+sed -i -e 's/28.0.4/29.1.2/g' \
+       -e 's/4b347a2b83221952cab93197f6e9bc7ffe54dd4bd0a9644c176aecde551721ca/f11949f3d8dcbcbd6d5de0b345b5ec8dbdd74572c6ce614c7b6e7551829d8035/g' \
        -e 's/PKG_GIT_REF:=v\$(PKG_VERSION)/PKG_GIT_REF:=docker-v$(PKG_VERSION)/' \
-       -e 's/6430e49/9a84135/g' ./feeds/packages/utils/dockerd/Makefile
+       -e 's/6430e49/de45c2a/g' ./feeds/packages/utils/dockerd/Makefile
 
 # docker
-# wget https://codeload.github.com/docker/cli/tar.gz/v29.1.1
-sed -i -e 's/28.0.4/29.1.1/g' \
-       -e 's/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/a02081b7d6fb10bfbc8afb621e7edc5124048b31eea7a1ab73c7ccd924b03a66/g' \
-       -e 's/b8034c0/0aedba5/g' ./feeds/packages/utils/docker/Makefile
+# wget https://codeload.github.com/docker/cli/tar.gz/v29.1.2
+sed -i -e 's/28.0.4/29.1.2/g' \
+       -e 's/09b41aa5ff656bc135feb80cb9b73c70aeba099ef9756c3cef7bcb2eb3c98ba6/35bee524e9b37573655f939ab8e16285361827e947fe047df06b6af32c3d18c3/g' \
+       -e 's/b8034c0/890dcca/g' ./feeds/packages/utils/docker/Makefile
 
 # containerd      
 # wget https://codeload.github.com/containerd/containerd/tar.gz/v2.2.0
