bf784f0e71347b6d910018658cd10449887658e4
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-01-17 05:35:50 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -43,8 +43,9 @@ mkdir -p ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/backgroun
 sed -i 's/6.6/6.12/g'  ./target/linux/x86/Makefile
 
 # zh netdata
-rm -rf ./feeds/luci/applications/luci-app-netdata/
-git clone https://github.com/Jason6111/luci-app-netdata ./feeds/luci/applications/luci-app-netdata/
+sed -i 's/1.3.1/2.8.5/g' ./feeds/packages/admin/netdata/Makefile
+#rm -rf ./feeds/luci/applications/luci-app-netdata/
+#git clone https://github.com/Jason6111/luci-app-netdata ./feeds/luci/applications/luci-app-netdata/
 
 # 开启netdata温控监测
 sed -i 's/charts\.d = no/charts\.d = yes/g' ./feeds/luci/applications/luci-app-netdata/root/etc/netdata/netdata.conf
