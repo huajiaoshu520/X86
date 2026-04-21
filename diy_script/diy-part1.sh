798efd9ada88c78120b581ec20adb09cf568e097
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-04-21 18:35:56 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -19,12 +19,13 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
 #sed -i 's/23\.05/25.12/g' feeds.conf.default
 #echo 'src-git helloworld https://github.com/huajiaoshu/helloworld' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git lucky https://github.com/Jason6111/luci-app-lucky' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 
 # Temp
-sed -i 's/coolsnowwolf\/luci/Jason6111\/luci/g' feeds.conf.default
+#sed -i 's/coolsnowwolf\/luci/Jason6111\/luci/g' feeds.conf.default
 #rm -rf ./target/linux/generic/hack-6.12/220-arm-gc_sections.patch
 # Switch to the specific commit (4bb635d) for mbedtls directory
 #rm -rf ./package/libs/mbedtls/patches/100-fix-gcc14-build.patch
